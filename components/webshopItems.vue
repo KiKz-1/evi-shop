9fa84e814fa2b46bf090afaff48311a67c7530c2
Martijn Prikkel <prikkel@evident.nl>
2020-12-23 00:29:40 +0100

    name change

--- components/webshopItems.vue
+++ components/webshopItems.vue
@@ -12,7 +12,7 @@
 
 <script>
 import WebshopItems from '~/components/webshopItems.vue';
-import fakeItems from '~/assets/fakeitems.json'
+import fakeItems from '~/assets/fakeItems.json'
 
 export default {
   components: {
