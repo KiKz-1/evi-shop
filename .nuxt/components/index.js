bfcf621be64fa391ddf6d4e2313c380ad9c66b86
EVIDENT\didonato <didonato@evident.nl>
2021-01-20 18:31:59 +0100

    remove .nuxt and add it to gitignore

--- .nuxt/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Header } from '../..\\components\\header.vue'
-export { default as WebshopItems } from '../..\\components\\webshopItems.vue'
-
-export const LazyHeader = import('../..\\components\\header.vue' /* webpackChunkName: "components_header" */).then(c => c.default || c)
-export const LazyWebshopItems = import('../..\\components\\webshopItems.vue' /* webpackChunkName: "components_webshopItems" */).then(c => c.default || c)
